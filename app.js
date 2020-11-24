8584ba260a17d6259d95b0f335e7e3c97b34ad9c
minhkhangleit <minhkhangleit>
2020-11-24 15:10:12 +0700

    init server FAQ project

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+
+// app
+const app = express();
