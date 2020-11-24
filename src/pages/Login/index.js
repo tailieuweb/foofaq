96d38893734f2840c1458baf139dfde8ee24bab5
logiangvuong225-20 <68698013+logiangvuong225-20@users.noreply.github.com>
2020-11-24 21:24:51 +0700

    fix google login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,18 +1,20 @@
 import React from "react";
 // import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
 // import GoogleLogin from "react-google-login";
-// import { useHistory, Redirect } from "react-router-dom";
+import { useHistory, Redirect } from "react-router-dom";
 
 // styles
 import "./index.scss";
-import { GoogleWLogin } from "../../provider/login";
+import { GoogleLogin } from "../../provider/login";
+import Button from "@material-ui/core/Button";
+
 // images
 // import GoogleIcon from "../../images/google_icon.svg";
 // import FacebookIcon from "../../images/facebook_icon.svg";
 // import InputLogin from "./InputLogin";
 
 const Login = () => {
-  // const history = useHistory();
+  const history = useHistory();
 
   // const responseFacebook = async (res) => {
   //   if (res.status !== "unknown") {
@@ -22,15 +24,15 @@ const Login = () => {
   //   }
   // };
 
-  // const responseGoogle = async (res) => {
-  //   if (res.profileObj.name) {
-  //     localStorage.setItem("accessToken", res.accessToken);
-  //     history.replace("/");
-  //     console.log(res);
-  //   }
-  // };
+  const handlerGoogleLogin = async (res) => {
+    if (res.profileObj.name) {
+      localStorage.setItem("accessToken", res.accessToken);
+      history.replace("/");
+      console.log(res);
+    }
+  };
 
-  // if (localStorage.getItem("accessToken")) return <Redirect to="/" />;
+  if (localStorage.getItem("accessToken")) return <Redirect to="/" />;
 
   return (
     <div className="LoginContainer">
@@ -38,8 +40,7 @@ const Login = () => {
         <div className="LeftSection"></div>
         <div className="RightSection">
           <h2 className="LoginTitle">Login With</h2>
-
-          <GoogleWLogin></GoogleWLogin>
+          <GoogleLogin handlerLogin={handlerGoogleLogin}></GoogleLogin>
         </div>
       </div>
     </div>
