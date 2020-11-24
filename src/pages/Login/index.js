6534ea3729650a3ebb6ed59b389f68622212de1d
Nguyễn Minh Triết <minhtriet2104.it@gmail.com>
2020-11-24 20:43:45 +0700

    Revert "created Login with Facebook"
    This reverts commit 3d6640a751c6bafa0bf709985ea53cca07de40e5.

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,26 +1,26 @@
 import React from "react";
-import FacebookLogin from "react-facebook-login";
+// import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
 // import GoogleLogin from "react-google-login";
-import { useHistory } from "react-router-dom";
+// import { useHistory, Redirect } from "react-router-dom";
 
 // styles
 import "./index.scss";
 
 // images
 // import GoogleIcon from "../../images/google_icon.svg";
-import FacebookIcon from "../../images/facebook_icon.svg";
+// import FacebookIcon from "../../images/facebook_icon.svg";
 // import InputLogin from "./InputLogin";
 
 const Login = () => {
-  const history = useHistory();
+  // const history = useHistory();
 
-  const responseFacebook = async (res) => {
-    if (res.status !== "unknown") {
-      // localStorage.setItem("accessToken", res.accessToken);
-      history.replace("/");
-      console.log(res);
-    }
-  };
+  // const responseFacebook = async (res) => {
+  //   if (res.status !== "unknown") {
+  //     localStorage.setItem("accessToken", res.accessToken);
+  //     history.replace("/");
+  //     console.log(res);
+  //   }
+  // };
 
   // const responseGoogle = async (res) => {
   //   if (res.profileObj.name) {
@@ -39,26 +39,11 @@ const Login = () => {
         <div className="RightSection">
           <h2 className="LoginTitle">Login With</h2>
           {/* Google */}
-          Code Google Login here
-          <FacebookLogin
-            appId="187968122879403"
-            textButton="FACEBOOK"
-            fields="name,email,picture"
-            callback={responseFacebook}
-            render={() => (
-              <button className="gg-login-btn">
-                <span
-                  className="icon"
-                  style={{
-                    backgroundImage: `url(${FacebookIcon})`,
-                  }}
-                ></span>
-                <span className="text">Facebook</span>
-              </button>
-            )}
-          />
+          // Code Google Login here
+          {/* Facebook */}
+          // Code Facebook Login here
           {/* Github */}
-          Code Github Login here
+          // Code Github Login here
         </div>
       </div>
     </div>
