37e7d519c2a2aa21b40185a4b9799325500c3b61
Huytue <bachvonam1996@gmail.com>
2020-11-27 20:34:41 +0700

    update validate data form

--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,22 @@
 const express = require("express");
-const router = require("express-promise-router");
+const router = require("express-promise-router")();
 const UserController = require("../controllers/user");
+const {
+	validateBody,
+	validateParam,
+	schemas
+} = require('../helpers/routerHelpers')
 
 //Route get all user
-router.route("/").get(UserController.index);
-
+router.route('/').get(UserController.index)
 //Route create user
-router.route("/").post(UserController.newUser);
+router.route('/').post(validateBody(schemas.userSchema), UserController.newUser)
 
 //Route get detail a user
-router.route("/:userID").get(UserController.getUser);
+router.route('/:userID').get(validateParam(schemas.idSchema, 'userID'), UserController.getUser)
 //Route relace user
-router.route("/:userID").put(UserController.replaceUser);
+router.route('/:userID').put(validateParam(schemas.idSchema, 'userID'), validateBody(schemas.userSchema), UserController.replaceUser)
 //Route update user
-router.route("/:userID").patch(UserController.updateUser);
+router.route('/:userID').patch(validateParam(schemas.idSchema, 'userID'), validateBody(schemas.userOptionalSchema), UserController.updateUser)
 
 module.exports = router
