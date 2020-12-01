c3e5309d4cd7f301302f8377fe78c7ae1c66bf7c
Nguyễn Thị Ngọc Huyền <ntnh5600@gmail.com>
2020-12-01 22:16:39 +0700

    active, inactive navigation bar

---
+++ src/pages/QuestionCategoryList/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+function CategoriesList(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default CategoriesList;
