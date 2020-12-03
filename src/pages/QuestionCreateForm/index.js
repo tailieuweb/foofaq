c0d7eb7daf2ffd80cf149553951c2a630c71a880
Sun1109 <44943684+Sun1109@users.noreply.github.com>
2020-12-03 14:26:18 +0700

    Revert "Choose PUT Question or POST Question"
    This reverts commit 678ce8e86509f32ae477b38bda6a6c575f5f0cbc, reversing
    changes made to d8523992aa5fdabf557afd06d3cc8e2e94d4b58a.

--- src/pages/QuestionCreateForm/index.js
+++ src/pages/QuestionCreateForm/index.js
@@ -1,17 +1,1 @@
-//import react
-import React from "react";
 
-//import component
-import NavigationBar from "../../components/NavigationBar";
-import QuestionForm from "../../components/QuestionForm";
-
-function QuestionCreateForm() {
-    return(
-        <div>
-            <NavigationBar></NavigationBar>
-            <QuestionForm id={1}></QuestionForm>
-        </div>
-    )
-}
-
-export default QuestionCreateForm;
