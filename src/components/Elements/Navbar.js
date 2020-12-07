2a6f107410fffcfccbb9afcbd93e6df16f808341
duclonggitnore <duclongsew@gmail.com>
2020-12-07 17:33:13 +0700

    init short, navbar

--- src/components/Elements/Navbar.js
+++ src/components/Elements/Navbar.js
@@ -1,21 +1,21 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 
-// const Navbar = () => {
-//   return (
-//     <nav className="navbar shadow fixed-top navbar-expand-sm navbar-dark bg-primary">
-//       <div className="container">
-//         <Link to="/" className="navbar-brand">
-//           Contact User
-//         </Link>
-//         <div>
-//           <Link to="/contacts/add" className="btn btn-light ml-auto">
-//             Create Contact
-//           </Link>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// };
+const Navbar = () => {
+  return (
+    <nav className="navbar shadow fixed-top navbar-expand-sm navbar-dark bg-primary">
+      <div className="container">
+        <Link to="/" className="navbar-brand">
+          Contact User
+        </Link>
+        <div>
+          <Link to="/contacts/add" className="btn btn-light ml-auto">
+            Create Contact
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-// export default Navbar;
+export default Navbar;
