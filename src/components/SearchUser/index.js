e26cada606b2dbee82113c2fb2479d572772c9af
duclonggitnore <duclongsew@gmail.com>
2020-12-08 11:20:26 +0700

    seacrh

---
+++ src/components/SearchUser/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
+import "./index.scss";
+
+function SearchUser(props) {
+  return (
+    <div className="search-container">
+      <input
+        type="text"
+        id="search-bar"
+        placeholder="Search..."
+        onChange={props.handleChangeSearch}
+      />
+      <button onClick={props.handleSearch} className="btn-search">
+        <SearchIcon className="search-icon"></SearchIcon>
+      </button>
+    </div>
+  );
+}
+
+export default SearchUser;
