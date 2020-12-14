e347a0895dba7e33b37834e0efbb25011fb1ca8d
Nguyễn Minh Triết <minhtriet2104.it@gmail.com>
2020-12-14 19:13:59 +0700

    init page: JobsGridView

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import "./index.scss";
 import reducer from "./reducers/index";
 
 // imports pages
-
+import JobsGridView from "./pages/JobsGridView";
 
 const composeEnhancers =
   (typeof window !== "undefined" &&
@@ -23,9 +23,7 @@ ReactDOM.render(
   <Provider store={store}>
     <Router>
       <Switch>
-
-        <Route exact path="" component={}></Route>
-        
+        <Route exact path="/jobs" component={JobsGridView}></Route>
       </Switch>
     </Router>
   </Provider>,
