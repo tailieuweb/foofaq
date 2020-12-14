2b536b36f10c7b97625840e90adfb239e138178d
logiangvuong225-20 <68698013+logiangvuong225-20@users.noreply.github.com>
2020-12-14 20:45:53 +0700

    QuestionApproval

--- src/pages/QuestionApproval/index.js
+++ src/pages/QuestionApproval/index.js
@@ -1,19 +1,13 @@
 import React from "react";
-import { Route } from "react-router-dom";
 import QuestionApprovalCard from "../../components/QuestionApprovalCard";
-import HeaderAsideNavbar from "../../components/HeaderAsideNavbar";
+import PageLayoutManager from "../../common/PageLayoutManager";
 const QuestionApproval = () => {
   return (
-    <div>
-      <div className="row">
-        <div className="col-md-2">
-          <HeaderAsideNavbar></HeaderAsideNavbar>
-        </div>
-        <div className="col-md-10">
-          <Route exact path="/approval" component={QuestionApprovalCard} />
-        </div>
-      </div>
-    </div>
+    <>
+      <PageLayoutManager>
+        <QuestionApprovalCard />
+      </PageLayoutManager>
+    </>
   );
 };
 
