eb83919b8566c975fbb043335bd553a3d8b22c5e
logiangvuong225-20 <68698013+logiangvuong225-20@users.noreply.github.com>
2020-12-15 08:30:49 +0700

    update JobEdit

--- src/pages/JobEdit/index.js
+++ src/pages/JobEdit/index.js
@@ -1,19 +1,15 @@
 import React from "react";
-import CategoriesForm from "../../components/FormManager/Categories";
 import JobsForm from "../../components/FormManager/Jobs";
-import EventFrom from "../../components/FormManager/Event";
-import HeaderAsideNavbar from "../../components/HeaderAsideNavbar";
 import PageLayout from "../../common/PageLayout";
 
 import { Route } from "react-router-dom";
 
-function FormManager(props) {
+function JobEdit(props) {
   return (
     <PageLayout>
       <div className="row">
-        <div className="col-md-2">
-        </div>
-        <div className="col-md-10">
+        <div className="col-md-3"></div>
+        <div className="col-md-9">
           <Route path="/forms/job/:id" component={JobsForm}></Route>{" "}
         </div>
       </div>
@@ -21,4 +17,4 @@ function FormManager(props) {
   );
 }
 
-export default FormManager;
+export default JobEdit;
