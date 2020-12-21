7e325432c71c4ce81379aff6c55c081dcbb549df
duclonggitnore <duclongsew@gmail.com>
2020-12-21 18:08:17 +0700

    form updateUser

--- src/index.js
+++ src/index.js
@@ -3,20 +3,12 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, compose, applyMiddleware } from "redux";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
 import thunk from "redux-thunk";
 import "./index.scss";
 import reducer from "./reducers/index";
-
-
 // imports pages:
-import QuestionManager from "./pages/QuestionManager";
 import Managers from "./pages/QuestionManager";
 import FormManager from "./pages/FormManager";
-//imports components:
-import UserDetailStoryFirst from "./components/UserDetailStoryFirst/index";
-
-
 const composeEnhancers =
   (typeof window !== "undefined" &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
@@ -27,14 +19,8 @@ ReactDOM.render(
   <Provider store={store}>
     <Router>
       <Switch>
-
-        <Route path="/form/" component={QuestionManager}></Route>
         <Route path="/manager/" component={Managers}></Route>
         <Route path="/forms/" component={FormManager}></Route>
-       
-
-        <Route path="/user-story/" component={UserDetailStoryFirst}></Route>
-
       </Switch>
     </Router>
   </Provider>,
