b1cddf571f9f8636ca2230f3ca77e62b69cd88c5
xuantri2000 <xuantri2000@gmail.com>
2020-12-28 07:52:23 +0700

    API update permission admin

--- routes/question.js
+++ routes/question.js
@@ -5,13 +5,13 @@ const UserController = require('../controllers/user')
 const { validate } = require("../validations/validation");
 
 //Route add Question
-router.post("/add",UserController.isAuth, validate.validateCreteQuestion(),questionController.createQuestion);
+router.post("/add",UserController.requireSignin, UserController.isAuth, validate.validateCreteQuestion(),questionController.createQuestion);
 //Route get Question
 router.get("/",questionController.getQuestion);
 //Route update Question
-router.put("/edit/:id",UserController.isAuth,validate.validateCreteQuestion(), questionController.editQuestion);
+router.put("/edit/:id",UserController.requireSignin, UserController.isAuth,validate.validateCreteQuestion(), questionController.editQuestion);
 //Route delete Question
-router.delete("/delete/:id",UserController.isAuth, questionController.deleteQuestion);
+router.delete("/delete/:id",UserController.requireSignin, UserController.isAuth, questionController.deleteQuestion);
 //Route detail Question
 router.get("/detail/:id", questionController.detailQuestion);
 module.exports = router;
--- routes/tag.js
+++ routes/tag.js
@@ -4,13 +4,13 @@ const tagController = require("../controllers/tag");
 const UserController = require('../controllers/user')
 
 //Route create Tag 
-router.post("/add", UserController.isAuth, tagController.createTag);
+router.post("/add", UserController.requireSignin, UserController.isAuth, tagController.createTag);
 //Route get all Tag 
 router.get("/", tagController.getAllTags);
 //Route edit Tag 
-router.put("/edit/:id", UserController.isAuth, tagController.editTag);
+router.put("/edit/:id", UserController.requireSignin, UserController.isAuth, tagController.editTag);
 //Route delete Tag 
-router.delete("/delete/:id", UserController.isAuth, tagController.deleleTag);
+router.delete("/delete/:id", UserController.requireSignin, UserController.isAuth, tagController.deleleTag);
 //Route detail Tag 
 router.get("/detail/:id", tagController.detailTag);
 module.exports = router;
