6e82934174d63f7d097a20255ab5c68f1c80b896
Trần Xuân Văn <50666377+xuanvan213@users.noreply.github.com>
2020-12-28 23:24:38 +0700

    changed name PageLayoutManager to PageManagerLayout

--- src/pages/EventsEdit/index.js
+++ src/pages/EventsEdit/index.js
@@ -9,7 +9,7 @@ import IconButton from "@material-ui/core/IconButton";
 import PhotoCamera from "@material-ui/icons/PhotoCamera";
 
 //components
-import HeaderAsideNavbar from "../../components/HeaderAsideNavbar";
+import PageManagerLayout from "../../common/PageManagerLayout";
 
 //styles
 import "./index.scss";
@@ -76,96 +76,88 @@ function Events(props) {
   }
   console.log(event.name);
   return (
-    <div>
-      <div className="row">
-        <div className="col-md-2">
-          <HeaderAsideNavbar></HeaderAsideNavbar>
-        </div>
-        <div className="col-md-10">
-          <h1>Event Form</h1>
-          <ul className="form-style-1">
-            <li>
-              <label>
-                Event Name <span className="required">*</span>
-              </label>
-              <input
-                type="text"
-                name="field1"
-                className="field-divided"
-                placeholder="name"
-                defaultValue={event.name}
-                onChange={(e) => {
-                  setName(e.target.value);
-                }}
-              />{" "}
-            </li>
+    <PageManagerLayout>
+      <h1>Event Form</h1>
+      <ul className="form-style-1">
+        <li>
+          <label>
+            Event Name <span className="required">*</span>
+          </label>
+          <input
+            type="text"
+            name="field1"
+            className="field-divided"
+            placeholder="name"
+            defaultValue={event.name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />{" "}
+        </li>
 
-            <li>
-              <input
-                accept="image/*"
-                className={classes.input}
-                id="contained-button-file"
-                multiple
-                type="file"
-                onChange={(e) => {
-                  setImage(e.target.value);
-                }}
-              />
-              <label htmlFor="contained-button-file">
-                <Button variant="contained" color="primary" component="span">
-                  Upload
-                </Button>
-              </label>
-              <input
-                accept="image/*"
-                className={classes.input}
-                id="icon-button-file"
-                type="file"
-                onChange={(e) => {
-                  setImage(e.target.value);
-                }}
-              />
-              <label htmlFor="icon-button-file">
-                <IconButton
-                  color="primary"
-                  aria-label="upload picture"
-                  component="span"
-                >
-                  <PhotoCamera />
-                </IconButton>
-              </label>
-            </li>
+        <li>
+          <input
+            accept="image/*"
+            className={classes.input}
+            id="contained-button-file"
+            multiple
+            type="file"
+            onChange={(e) => {
+              setImage(e.target.value);
+            }}
+          />
+          <label htmlFor="contained-button-file">
+            <Button variant="contained" color="primary" component="span">
+              Upload
+            </Button>
+          </label>
+          <input
+            accept="image/*"
+            className={classes.input}
+            id="icon-button-file"
+            type="file"
+            onChange={(e) => {
+              setImage(e.target.value);
+            }}
+          />
+          <label htmlFor="icon-button-file">
+            <IconButton
+              color="primary"
+              aria-label="upload picture"
+              component="span"
+            >
+              <PhotoCamera />
+            </IconButton>
+          </label>
+        </li>
 
-            <li>
-              <label>
-                Description <span className="required">*</span>
-              </label>
-              <textarea
-                name="field5"
-                id="field5"
-                className="field-long field-textarea"
-                defaultValue={event.description}
-                onChange={(e) => {
-                  setDescription(e.target.value);
-                }}
-              />
-            </li>
-            <li>
-              <Button onClick={handleSubmit} variant="outlined" color="primary">
-                {" "}
-                Send
-              </Button>
-            </li>
-          </ul>
-        </div>
-      </div>
-
+        <li>
+          <label>
+            Description <span className="required">*</span>
+          </label>
+          <textarea
+            name="field5"
+            id="field5"
+            className="field-long field-textarea"
+            defaultValue={event.description}
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+          />
+        </li>
+        <li>
+          <Button onClick={handleSubmit} variant="outlined" color="primary">
+            {" "}
+            Send
+          </Button>
+        </li>
+      </ul>
       <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
         <Alert onClose={handleClose} severity="success">
           This is a success message!
         </Alert>
       </Snackbar>
-    </div>
+    </PageManagerLayout>
   );
 }
 
