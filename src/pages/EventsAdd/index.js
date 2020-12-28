3acb6898552a7fa6158bf5ac69c943e9ffbc36ba
Trần Xuân Văn <50666377+xuanvan213@users.noreply.github.com>
2020-12-28 23:20:07 +0700

    changed name PageLayoutManager to PageManagerLayout

--- src/pages/EventsAdd/index.js
+++ src/pages/EventsAdd/index.js
@@ -8,7 +8,7 @@ import IconButton from "@material-ui/core/IconButton";
 import PhotoCamera from "@material-ui/icons/PhotoCamera";
 
 //components
-import PageLayoutManager from "../../common/PageLayoutManager";
+import PageLayoutManager from "../../common/PageManagerLayout";
 
 //Styles
 import "./index.scss";
