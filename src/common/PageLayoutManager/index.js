b5b4c9e241b1617881d34202e4be9290842f3078
Sun1109 <44943684+Sun1109@users.noreply.github.com>
2020-12-29 16:53:39 +0700

    Update PageLayoutManager

--- src/common/PageLayoutManager/index.js
+++ src/common/PageLayoutManager/index.js
@@ -1,13 +1,31 @@
 import React from "react";
 import HeaderAsideNavbar from "../../components/HeaderAsideNavbar";
+import { makeStyles } from "@material-ui/core/styles";
+const useStyles = makeStyles({
+  headersideBar: (propsS) => ({
+    width: "18%",
+    float: propsS.float,
+    display: propsS.display,
+  }),
+
+  bodySideBar: (propsS) => ({
+    float: propsS.float,
+    width: "75%",
+    marginLeft: "50px",
+  }),
+});
+
 const PageLayoutManager = (props) => {
+  const propsS = { float: `${props.float}`, display: props.display };
+  const classes = useStyles(propsS);
+
   return (
     <>
-      <div className="row">
-        <div className="col-md-2">
+      <div className="headerSidebNavbar">
+        <div className={classes.headersideBar}>
           <HeaderAsideNavbar />
         </div>
-        <div className="col-md-10">{props.children}</div>
+        <div className={classes.bodySideBar}>{props.children}</div>
       </div>
     </>
   );
