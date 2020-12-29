b53ca78b0e421ba212f8d9afadf704001b49e134
Sun1109 <44943684+Sun1109@users.noreply.github.com>
2020-12-29 17:31:53 +0700

    update page QuestionAdd

--- src/components/CategoriesInput/index.js
+++ src/components/CategoriesInput/index.js
@@ -3,64 +3,13 @@ import TextField from "@material-ui/core/TextField";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import CheckBoxOutlineBlankIcon from "@material-ui/icons/CheckBoxOutlineBlank";
 import CheckBoxIcon from "@material-ui/icons/CheckBox";
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-// import axios from "axios";
-
-// import { pagCategories,getQuestionForCate } from "../../helpers";
+import React from "react";
 
 const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
 const checkedIcon = <CheckBoxIcon fontSize="small" />;
 
 function CategoriesInput({ listCategories, setTextCate, arr }) {
-  // const [cagtegories,setCategories]=useState([]);
-  // const [textCate,setTextCate] = useState([]);
-  // const [question,setQuestion] = useState([]);
-
-  // //get categories
-  // useEffect(()=>{
-  //     (async () => {
-  //         const result = await pagCategories();
-  //         setCategories(result);
-  //       })();
-  // },[])
-
-  // //get question để lấy id cuối
-  // useEffect(()=>{
-  //     (async () => {
-  //         const result = await getQuestionForCate();
-  //         setQuestion(result);
-  //       })();
-  // },[])
-
-  // //lấy id cuối
-  // const [idTest,setIdTest]=useState([]);
-  //   useEffect(()=>{
-  //     question.map((q)=>{
-  //         console.log(q.id);
-  //      setIdTest(q);
-  //     })
-  //   },[question])
-  //   const [id,setId]=useState(null);
-  // useEffect(()=>{
-  //     setId(parseInt(idTest.id)+1);
-  // // id +1 ;
-  // },[idTest])
-  // console.log(id);
-  // console.log(idTest);
-  // setIdR(Number(idTest) +1);
-  //post categories với id +1
-  // post question rồi post cateogries
-  // const handleAuto =()=>{
-  //     textCate.map((tx)=>{
-  //         axios
-  //         .post(`https://5fc48ee536bc790016343a0b.mockapi.io/questions/${id}/categories`, {
-  //             questionId: id,
-  //             name: tx.name
-  //         })
-  //     })
-  // }
-
+  
   return (
     <>
       <Autocomplete
