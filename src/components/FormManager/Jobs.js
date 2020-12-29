5fca40945f2ed71e988b6cc3534316f5d998e8c5
Sun1109 <44943684+Sun1109@users.noreply.github.com>
2020-12-29 18:35:36 +0700

    update page JobGridViewManager

--- src/components/FormManager/Jobs.js
+++ src/components/FormManager/Jobs.js
@@ -1,15 +1,58 @@
 import React, { useState, useEffect } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import TextField from "@material-ui/core/TextField";
-import Icon from "@material-ui/core/Icon";
-import Snackbar from "@material-ui/core/Snackbar";
-import Alert from "@material-ui/lab/Alert";
 import { useParams } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import "./Jos.scss";
 import { getJob, addJobs, updateJobs } from "../../helpers";
 
+import { withStyles } from "@material-ui/core/styles";
+import Dialog from "@material-ui/core/Dialog";
+import MuiDialogTitle from "@material-ui/core/DialogTitle";
+import MuiDialogContent from "@material-ui/core/DialogContent";
+import MuiDialogActions from "@material-ui/core/DialogActions";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+import Typography from "@material-ui/core/Typography";
+import SendIcon from "@material-ui/icons/Send";
+
+import Link from "../../common/CustomLink";
+
+import { styles } from "./classesJob";
+
+const DialogTitle = withStyles(styles)((props) => {
+  const { children, classes, onClose, ...other } = props;
+
+  return (
+    <MuiDialogTitle disableTypography className={classes.root} {...other}>
+      <Typography variant="h6">{children}</Typography>
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          className={classes.closeButton}
+          onClick={onClose}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
+});
+
+const DialogContent = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiDialogContent);
+
+const DialogActions = withStyles((theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(1),
+  },
+}))(MuiDialogActions);
+
+
 function Jobs(props) {
+  const [nofi, setNofi] = useState("");
   const [open, setOpen] = React.useState(false);
   // name, description, type, area, company, experience, role
   const [job, setJob] = useState([]);
@@ -25,45 +68,72 @@ function Jobs(props) {
   let handleSubmit = (event) => {
     event.preventDefault();
   };
-  const handleClose = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
 
+  const handleClose = () => {
     setOpen(false);
   };
+
   useEffect(() => {
     (async () => {
       const questionData = await getJob(id);
       setJob(questionData);
     })();
   }, []);
+
+  const check = () => {
+    if(name.length == "" || description.length == "" || type.length == "" || area.length == "" || company.length == "" || experience.length == "" || role.length == "")
+    {
+      return true;
+    }
+    else{
+      return false
+    }
+  }
+
   if (id === undefined) {
     handleSubmit = (event) => {
-      event.preventDefault();
-      addJobs(name, description, type, area, company, experience, role)
+      if(check() == true){
+        setOpen(true);
+        setNofi("Failed");
+      }
+      else{
+        event.preventDefault();
+        addJobs(name, description, type, area, company, experience, role)
         .then(function (response) {
           setOpen(true);
           //window.location.reload();
+          setNofi("Successfully");
         })
         .catch(function (error) {
           // handle error
           console.log(error);
+          setOpen(true);
+          setNofi("Failed");
         });
+      }
     };
   } else {
     handleSubmit = (event) => {
-      event.preventDefault();
-      updateJobs(id, name, description, type, area, company, experience, role)
+      if(check() == true){
+        setOpen(true);
+        setNofi("Failed");
+      }
+      else{
+        event.preventDefault();
+        updateJobs(id, name, description, type, area, company, experience, role)
         .then(function (response) {
           setOpen(true);
-          window.location.reload();
+          //window.location.reload();
+          setNofi("Successfully");
         })
         .catch(function (error) {
           // handle error
           console.log(error);
+          setOpen(true);
+          setNofi("Failed");
         });
-    };
+      };
+    }
   }
   return (
     <div>
@@ -74,14 +144,15 @@ function Jobs(props) {
             Jobs Name <span className="required">*</span>
           </label>
           <input
+          required
             type="text"
             name="field1"
             className="field-divided"
-            placeholder="name"
+            placeholder="Input name..."
             defaultValue={job.name}
             onChange={(e) => {
               setName(e.target.value);
-            }}
+            }} 
           />{" "}
         </li>
         <li>
@@ -92,10 +163,11 @@ function Jobs(props) {
             type="text"
             name="field3"
             className="field-long"
+            placeholder="Input type..."
             defaultValue={job.type}
             onChange={(e) => {
               setType(e.target.value);
-            }}
+            }} required
           />
         </li>
         <li>
@@ -106,10 +178,11 @@ function Jobs(props) {
             type="text"
             name="field3"
             className="field-long"
+            placeholder="Input area..."
             defaultValue={job.area}
             onChange={(e) => {
               setArea(e.target.value);
-            }}
+            }} required
           />
         </li>
         <li>
@@ -120,24 +193,26 @@ function Jobs(props) {
             type="text"
             name="field3"
             className="field-long"
+            placeholder="Input company..."
             defaultValue={job.company}
             onChange={(e) => {
               setCompany(e.target.value);
-            }}
+            }} required
           />
         </li>
         <li>
           <label>
-            experience <span className="required">*</span>
+            Experience <span className="required">*</span>
           </label>
           <input
             type="text"
             name="field3"
             className="field-long"
+            placeholder="Input experience..."
             defaultValue={job.experience}
             onChange={(e) => {
               setExperience(e.target.value);
-            }}
+            }} required
           />
         </li>
 
@@ -149,10 +224,12 @@ function Jobs(props) {
             type="text"
             name="field3"
             className="field-long"
+            placeholder="Input role..."
             defaultValue={job.role}
             onChange={(e) => {
               setRole(e.target.value);
-            }}
+            }} 
+            required
           />
         </li>
 
@@ -164,25 +241,42 @@ function Jobs(props) {
             name="field5"
             id="field5"
             className="field-long field-textarea"
+            placeholder="Input description..."
             defaultValue={job.description}
             onChange={(e) => {
               setDescription(e.target.value);
-            }}
+            }} 
+            required
           />
         </li>
         <li>
-          <Button onClick={handleSubmit} variant="outlined" color="primary">
-            {" "}
-            Send
+          <Button onClick={handleSubmit} variant="outlined" color="primary" startIcon={<SendIcon />}>
+            {/* {" "}
+            Send */}
           </Button>
         </li>
       </ul>
 
-      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-        <Alert onClose={handleClose} severity="success">
-          This is a success message!
-        </Alert>
-      </Snackbar>
+      <Dialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+          Notification
+        </DialogTitle>
+        <DialogContent dividers>
+          <Typography gutterBottom>{nofi}</Typography>
+        </DialogContent>
+        <DialogActions>
+          <Link to={"/manager/jobs"}>
+            <Button autoFocus onClick={handleClose} color="primary">
+              OK
+            </Button>
+          </Link>
+        </DialogActions>
+      </Dialog>
+      
     </div>
   );
 }
