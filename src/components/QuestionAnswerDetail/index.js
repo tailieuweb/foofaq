09da59f8a65c5ebd09c2e2b1b4e0aa40b1a3ed54
Trần Xuân Văn <50666377+xuanvan213@users.noreply.github.com>
2020-12-29 13:20:29 +0700

    detach classes to file classes.js

--- src/components/QuestionAnswerDetail/index.js
+++ src/components/QuestionAnswerDetail/index.js
@@ -7,7 +7,6 @@ import Link from "../../common/CustomLink";
 
 //material UI
 import Skeleton from "@material-ui/lab/Skeleton";
-import { makeStyles } from "@material-ui/core/styles";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import Avatar from "@material-ui/core/Avatar";
@@ -22,47 +21,13 @@ import DoneIcon from "@material-ui/icons/Done";
 import Snackbar from "@material-ui/core/Snackbar";
 import MuiAlert from "@material-ui/lab/Alert";
 
+//style
+import useStyles from "./classes";
+
 //APIS
 import { getAnswers } from "../../helpers";
 import { useParams } from "react-router-dom";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    margin: "2% auto",
-    flexGrow: 1,
-    maxWidth: "89.5%",
-    width: "89.5%",
-    display: "block",
-    "& > *": {
-      margin: theme.spacing(1),
-    },
-  },
-  userAnswerInfo: {
-    backgroundColor: "#D5E8D4",
-  },
-  vote: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "flex-start",
-    height: 30,
-  },
-  buttonEdit: {
-    float: "right",
-    position: "relative",
-    right: "2%",
-    bottom: "43px",
-    display: "block",
-  },
-  doneIcon: {
-    width: "100%",
-    height: "50px",
-    color: "green",
-    display: "block",
-    position: "relative",
-    top: "24%",
-  },
-}));
 function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
--- src/pages/QuestionDetail/index.js
+++ src/pages/QuestionDetail/index.js
@@ -5,7 +5,7 @@ import Cookies from "js-cookie";
 
 //import style
 import "./index.scss";
-import { makeStyles } from "@material-ui/core/styles";
+import useStyles from "./classes";
 
 //material
 import Paper from "@material-ui/core/Paper";
@@ -21,32 +21,6 @@ import { Grid } from "@material-ui/core";
 
 //APIS
 import { getQuesitonById, getAnswers } from "../../helpers";
-
-//style
-const useStyles = makeStyles((theme) => ({
-  questionInfo: {
-    margin: "5%",
-  },
-  answerDetail: {
-    margin: "5%",
-    border: "1px solid #ced4da",
-    width: "89.5%",
-    borderRadius: " 15px",
-    position: "relative",
-  },
-  answersText: {
-    position: "absolute",
-    top: "-5%",
-    left: "1%",
-    backgroundColor: "#28a745",
-    color: "#fff",
-    padding: "5px 15px",
-    borderRadius: "5px",
-  },
-  skeletion: {
-    margin: "0.5rem auto",
-  },
-}));
 
 const QuestionDetail = (props) => {
   const classes = useStyles();
