a3912445d326bd5c6f5b2144c25fb0d5689f9aae
Trần Xuân Văn <50666377+xuanvan213@users.noreply.github.com>
2020-12-29 15:52:33 +0700

    update EventGridViewPage, remove page EventsList

--- src/components/EventsGridView/index.js
+++ src/components/EventsGridView/index.js
@@ -1,51 +1,75 @@
-import * as React from "react";
-import Button from "@material-ui/core/Button";
+import React, { useState, useEffect } from "react";
+
+//Material UI
 import { DataGrid } from "@material-ui/data-grid";
 
+//components
+import SearchBar from "../../components/SearchBar";
+
+//APIs
+import { getEventsSearch } from "../../helpers";
 
 export default function EventsGridView({ extraColumns, extraRows }) {
+  const [eventData, setEventData] = useState([]);
+
+  const [keyword, setKeyword] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const eventData = await getEventsSearch(keyword);
+      setEventData(eventData);
+    })();
+  }, [keyword]);
+  const handleChangeSearch = (e) => {
+    setKeyword(e.target.value);
+  };
+  const handleSearch = () => {
+    setKeyword(keyword);
+  };
+
+  let rows = eventData;
   let columns = [
+    { field: "id", headerName: "ID" },
+    // { field: 'createdAt', width: 210 },
+    { field: "date", headerName: "Date", width: 200 },
+    { field: "name", width: 200, headerName: "Name" },
     {
-      field: "date",
-      headerName: "Year",
-      width: 150,
+      field: "imageUri",
+      headerName: "Image",
+      width: 110,
       renderCell: (params) => (
         <strong>
-          {params.value.getFullYear()}
-          <Button
-            variant="contained"
-            color="primary"
-            size="small"
-            style={{ marginLeft: 16 }}
-          >
-            Open
-          </Button>
+          <div className="aroundImageEvents">
+            <img src={params.value} alt="hehe" />
+          </div>
         </strong>
       ),
     },
+    { field: "description", headerName: "Description", width: 400 },
+    { field: "place", headerName: "Place", width: 200 },
   ];
-  
-  let rows = [
-    {
-      id: 1,
-      date: new Date(1979, 0, 1),
-    },
-    {
-      id: 2,
-      date: new Date(1984, 1, 1),
-    },
-    {
-      id: 3,
-      date: new Date(1992, 2, 1),
-    },
-  ];  
 
   columns = extraColumns ? [...columns, ...extraColumns] : columns;
   rows = extraRows ? [...rows, ...extraRows] : rows;
 
   return (
-    <div style={{ height: 300, width: "100%" }}>
-      <DataGrid rows={rows} columns={columns} />
+    <div style={{ height: 500, width: "100%" }}>
+      <h1>Events</h1>
+      <SearchBar
+        handleChangeSearch={handleChangeSearch}
+        handleSearch={handleSearch}
+      />
+      <br />
+      <br />
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={6}
+        rowsPerPageOptions={[5, 10, 20]}
+        pagination
+        {...rows}
+        rowHeight={80}
+      />
     </div>
   );
 }
